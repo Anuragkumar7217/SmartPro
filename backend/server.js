019a614a973677f198a0d3f6e9fccd4c9f4d10cf
Anurag Kumar <anuragkumar7217@gmail.com>
2026-07-21 17:14:51 +0530

    First server will start then mongodb

--- backend/server.js
+++ backend/server.js
@@ -6,16 +6,25 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 
-const startServer = async () => {
-  // Wait for database connection first
-  await connectDB();
-
-  // Start listening only after DB is connected
+const startServer = () => {
+  // 1. Port pehle listen karo taaki Render ko 502 Error na dena pade
   app.listen(PORT, "0.0.0.0", () => {
-    // app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
+  // 2. MongoDB background me connect hone do
+  connectDB();
 };
+
+// const startServer = async () => {
+//   // Wait for database connection first
+//   await connectDB();
+
+//   // Start listening only after DB is connected
+//   app.listen(PORT, "0.0.0.0", () => {
+//     // app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+//   });
+// };
 
 startServer();
 
